23e3d951016102a1bb4f3bee1223dd57f3463145
Franz Joshua Dela Cruz <franzdc.21@gmail.com>
2024-11-06 23:45:16 +0800

    chore: remove unused container component

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,3 @@
-export { default as Container } from './Container';
 export { default as PlayerControls } from './PlayerControls';
 export { default as PlayerExtraActions } from './PlayerExtraActions';
 export { default as PlayerSeekBar } from './PlayerSeekBar';
