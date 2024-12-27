1fc300e82cc89df1784cf4fae34625237ad869ab
Franz Joshua Dela Cruz <franzdc.21@gmail.com>
2024-12-27 14:42:34 +0800

    chore: remove unused hooks

--- hooks/useRepeat.ts
+++ hooks/useRepeat.ts
@@ -5,36 +5,13 @@ import { getRepeatMode, switchRepeat } from '@/services';
 const QUERY_KEY = ['repeatMode'];
 
 /**
- * Hook to get the repeat mode state.
- *
- * Repeat mode state is not polled to not overload with requests.
+ * Hook to manipulate the repeat mode state.
+ * State is not polled to not overload with requests.
  *
  * The repeat mode state is only updated when:
  * - The app is opened
  * - The repeat mode is changed
  */
-export const useRepeatModeState = () =>
-  useQuery({
-    queryKey: QUERY_KEY,
-    queryFn: getRepeatMode,
-    initialData: { mode: 'NONE' },
-  });
-
-export const useSwitchRepeat = () => {
-  const queryClient = useQueryClient();
-
-  return useMutation({
-    mutationKey: QUERY_KEY,
-    mutationFn: switchRepeat,
-    onSuccess: () => {
-      // Invalidate the query to refetch the data
-      queryClient.invalidateQueries({
-        queryKey: QUERY_KEY,
-      });
-    },
-  });
-};
-
 export const useRepeat = () => {
   const queryClient = useQueryClient();
 
